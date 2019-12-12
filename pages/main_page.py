c5610d0fe35a29097f241b0a62a5e1ada90dd3be
Valera <valera.vengo@gmail.com>
2019-12-12 23:01:34 +0500

    Changed files main_page.py, test_main_page.py. Worked with pagetransitions

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,12 +1,19 @@
 ﻿from .base_page import BasePage
 from selenium.webdriver.common.by import By
-# from .locators import MainPageLocators
+from .locators import MainPageLocators
+from .login_page import LoginPage
 
 class MainPage(BasePage): 
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        #login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         # assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
-        login_link.click()
+        #login_link.click()
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
+        # return LoginPage(browser=self.browser, url=self.browser.current_url)
+        #alert = self.browser.switch_to.alert
+        #alert.accept()
+
     def should_be_login_link(self):
         # elf.browser.find_element(By.CSS_SELECTOR, "#login_link")
         assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
